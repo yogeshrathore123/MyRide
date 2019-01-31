780882d1ae3b78cae6c0ff8ae56b099568ab48cc
yogesh <yogesh-rathore@nissanmotor.com>
2019-01-31 14:20:58 +0530

    Added UIView Extension

---
+++ MyRide/Extensions/UIViewExt.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewExt.swift
+//  MyRide
+//
+//  Created by Yogesh Rathore on 31/01/19.
+//  Copyright © 2019 Nissan Digital Hub. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func fadeTo(alphaValue: CGFloat, withDuration duration: TimeInterval)  {
+        UIView.animate(withDuration: duration) {
+            self.alpha = alphaValue
+        }
+    }
+}
